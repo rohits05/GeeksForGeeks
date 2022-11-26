ea393eba3761511f23764f296caa206cc947e5a5
Rohit Shaw <carpediem212045@gmail.com>
2022-11-27 00:17:45 +0530

    Updated Solution - GfG to GitHub

--- School/Second Largest/second-largest.java
+++ School/Second Largest/second-largest.java
@@ -36,9 +36,14 @@ class Solution {
     int print2largest(int arr[], int n) {
         // code here
         // Without sorting
-        int maxI = Integer.MIN_VALUE, sLar = -1;
-        for(int i=0; i<n; i++) {  if(maxI < arr[i])  maxI = arr[i]; }
-        for(int i=0; i<n; i++) {  if(arr[i] > sLar && arr[i] < maxI) sLar = arr[i]; }
-        return sLar;
+        // int maxI = Integer.MIN_VALUE, sLar = -1;
+        // for(int i=0; i<n; i++) {  if(maxI < arr[i])  maxI = arr[i]; }
+        // for(int i=0; i<n; i++) {  if(arr[i] > sLar && arr[i] < maxI) sLar = arr[i]; }
+        // return sLar;
+        
+        // Using Sort MeTHoD
+        Arrays.sort(arr);
+        for(int i=n-1; i>0; i--){if(arr[i-1] != arr[i]) return arr[i-1];}
+        return -1;
     }
 }
