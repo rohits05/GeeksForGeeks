c5d343e0641145bf24095b3679ee14152acab8c4
Rohit Shaw <carpediem212045@gmail.com>
2022-12-06 07:40:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Segregate even and odd nodes in a Link List/segregate-even-and-odd-nodes-in-a-link-list.java
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+	static void printList(Node node, PrintWriter out) 
+	{ 
+		while (node != null) 
+		{ 
+			out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node ans = ob.divide(n, head);
+            printList(ans, out); 
+            t--;
+        }
+        out.flush();
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* 
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution{
+    Node divide(int N, Node head){
+    
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node prev = dummy;
+        Node temp = head;
+        
+         while(temp.next!=null && temp.data%2==0){
+                    temp = temp.next;
+                    prev = prev.next;   
+                }
+                Node oddHead = temp;
+                Node p = temp.next;
+                while(p!=null)
+                {
+                    if(p.data%2==0)
+                    {
+                        prev.next = p;
+                        prev = prev.next;
+                    }else{
+                        temp.next = p;
+                        temp = temp.next;
+                    }
+                            p = p.next;
+         }
+        temp.next = null;
+        prev.next = oddHead;
+     
+        return dummy.next;
+    }
+}
