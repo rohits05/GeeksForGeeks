0e23361089c482444b6f95df8d4467417c8d9a3b
Rohit Shaw <carpediem212045@gmail.com>
2022-12-08 21:56:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/3 Divisors/3-divisors.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int q = sc.nextInt();
+            ArrayList<Long> query = new ArrayList<>();
+            for(int i=0;i<q;i++){
+                query.add(sc.nextLong());
+            }
+            Solution ob = new Solution();
+                
+            ArrayList<Integer> ans = ob.threeDivisors(query,q);
+            for(int cnt : ans) {
+                System.out.println(cnt);
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+
+    static boolean isPrime(long a){
+        for(long f=2; f<a; f++) if(a % f == 0) return false;
+
+        return true;
+    }
+
+    static ArrayList<Integer> threeDivisors(ArrayList<Long> query, int q){
+        ArrayList<Integer> arr=new ArrayList<>();
+
+        for(Long qu: query){
+            int res = 0;
+
+            for(long i=2; i*i<=qu; i++) if(isPrime(i)) res++;
+            arr.add(res);
+        }
+        return arr;
+    }
+
+}
