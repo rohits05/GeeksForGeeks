9ace9a804483760dc144e0fd330d796cdfbff2ea
Rohit Shaw <carpediem212045@gmail.com>
2022-12-16 21:03:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Queue using stack/queue-using-stack.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class Solution
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			Queue g = new Queue();
+			
+			int q = sc.nextInt();
+			while(q>0)
+			{
+				int QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					g.enqueue(a);
+				}
+				else if(QueryType == 2)
+				System.out.print(g.dequeue()+" ");
+			q--;
+			}	
+			System.out.println();
+				
+			
+			
+		t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Queue
+{
+    Deque<Integer> input = new ArrayDeque<Integer>();
+    Deque<Integer> output = new ArrayDeque<Integer>(); 
+
+    int dequeue(){
+        return input.poll();
+    }
+
+    void enqueue(int x){    
+        input.offer(x);
+    }
+}
+
