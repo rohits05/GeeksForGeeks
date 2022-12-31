c25a9c7aab0adef98d9bb8f312cd90573feba840
Rohit Shaw <carpediem212045@gmail.com>
2022-12-31 09:30:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of Unique Paths/number-of-unique-paths.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking dimensions of the matrix
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    Solution ob = new Solution();
+		    //calling method NumberOfPath()
+		    System.out.println(ob.NumberOfPath(a,b));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find total number of unique paths.
+    public static int NumberOfPath(int a, int b) 
+    {
+        //Your code here
+        int n = (a+b)-2, r = a-1, res = 1;
+        
+        for(int i=1; i<=r; i++){
+            res = res * (n-r+i) / i;
+        }
+        return res;
+    }
+}
