662ea9acbf602af92302ad6960dab53dccc0261b
Rohit Shaw <carpediem212045@gmail.com>
2023-01-12 11:46:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimize the sum/minimize-the-sum.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    long minimizeSum(int N, int arr[]) {
+        // code here
+        PriorityQueue<Integer> q = new  PriorityQueue<Integer>();
+        int sum =0;
+        for(int i=0; i<N ; i++) q.add(arr[i]);
+        while(q.size() != 1){
+            int temp = q.poll() + q.poll();
+            sum += temp;
+            q.add(temp);
+        }
+        return (long)sum;
+    }
+}
+
+//{ Driver Code Starts.
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0; i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            long ans = obj.minimizeSum(n, A);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
